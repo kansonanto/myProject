369bb3d65dd302fd7ee38998654e9cf67db4de94
kansonanto <kansona@gmail.com>
2018-03-11 21:48:12 +0530

    new changess

--- app/report/controllers/reportController.js
+++ app/report/controllers/reportController.js
@@ -4,16 +4,41 @@ angular.module('myproject1App')
     .controller('reportController', ['$state', '$scope', '$q', 'userAPI', 'reportEntity', function ($state, reportscope, $q, userAPI, reportEntity) {
         var vm = this;
         vm.order = {};
-        vm.order.printBookName = '';        
-        vm.order.printOrderNumber = '';
-        vm.order.printReleaseNumber = '';
-        vm.order.printOrderDate = '';
-        vm.order.printReleaseDate = '';
-        vm.order.bookNoOfCopies = '';
+        vm.order.printBookName = 'SPBA 202 M.B.A 2nd  Year(3rd Semester) E/M';        
+        vm.order.printOrderNumber = '1211';
+        vm.order.printReleaseNumber = '1211';
+        vm.order.printOrderDate = new Date();
+        vm.order.printReleaseDate = new Date();
+        vm.order.bookNoOfCopies = '8000';
         vm.order.paymentType = '';
-        vm.order.bookType = '';
-        vm.order.noOfBRV = '';                
+        vm.order.bookType = 'Free copies';
+        vm.order.noOfBRV = '3';                
         reportscope.printBillingDate = new Date();
+
+        vm.order.invoiceNo = '68';
+        vm.order.printing1000PreRate = '2000';
+        vm.order.printingRate = '45000';
+        vm.order.plateMakingRate = '2000';
+        vm.order.kraftRate = '5000';
+        vm.order.transportRate = '1000';
+
+        vm.order.totalBillAmount = Number(vm.order.printingRate) + Number(vm.order.plateMakingRate) + Number(vm.order.kraftRate) + Number(vm.order.transportRate);
+
+        vm.order.totalAmountWord =   convertNumberToWords(Number(vm.order.totalBillAmount));
+
+
+        // paper board statement 
+        vm.order.paperPBIVNo = '1211';
+        vm.order.boardPBIVNo = '1212';
+        vm.order.paperPBIVDate = new Date();
+        vm.order.boardPBIVDate = new Date();
+        vm.order.paperPBIVSize = 'Paper 84CM Reel 19.5Kg 80Gsm Hi-tech Map litho';
+        vm.order.boardPBIVSize = '61x90cm 25.3Kg 230GSM Multi -layer Coated (Aura pearl)';
+        vm.order.paperPBIVConsumptionSize = '1211.12';
+        vm.order.boardPBIVReams = '200';
+        vm.order.boardPBIVSheets = '100';
+        vm.order.boardPBIVConsumptionSize = '12.11';
+
 
         reportscope.printOrderName = 'test report name dfdf d1';
         var base64Img = null;
@@ -38,6 +63,88 @@ angular.module('myproject1App')
             reportscope.reportName = reportEntity.getReportEntity().reportName;
 
 
+        }
+
+        function convertNumberToWords(amount) {
+            var words = new Array();
+            words[0] = '';
+            words[1] = 'One';
+            words[2] = 'Two';
+            words[3] = 'Three';
+            words[4] = 'Four';
+            words[5] = 'Five';
+            words[6] = 'Six';
+            words[7] = 'Seven';
+            words[8] = 'Eight';
+            words[9] = 'Nine';
+            words[10] = 'Ten';
+            words[11] = 'Eleven';
+            words[12] = 'Twelve';
+            words[13] = 'Thirteen';
+            words[14] = 'Fourteen';
+            words[15] = 'Fifteen';
+            words[16] = 'Sixteen';
+            words[17] = 'Seventeen';
+            words[18] = 'Eighteen';
+            words[19] = 'Nineteen';
+            words[20] = 'Twenty';
+            words[30] = 'Thirty';
+            words[40] = 'Forty';
+            words[50] = 'Fifty';
+            words[60] = 'Sixty';
+            words[70] = 'Seventy';
+            words[80] = 'Eighty';
+            words[90] = 'Ninety';
+            amount = amount.toString();
+            var atemp = amount.split(".");
+            var number = atemp[0].split(",").join("");
+            var n_length = number.length;
+            var words_string = "";
+            if (n_length <= 9) {
+                var n_array = new Array(0, 0, 0, 0, 0, 0, 0, 0, 0);
+                var received_n_array = new Array();
+                for (var i = 0; i < n_length; i++) {
+                    received_n_array[i] = number.substr(i, 1);
+                }
+                for (var i = 9 - n_length, j = 0; i < 9; i++, j++) {
+                    n_array[i] = received_n_array[j];
+                }
+                for (var i = 0, j = 1; i < 9; i++, j++) {
+                    if (i == 0 || i == 2 || i == 4 || i == 7) {
+                        if (n_array[i] == 1) {
+                            n_array[j] = 10 + parseInt(n_array[j]);
+                            n_array[i] = 0;
+                        }
+                    }
+                }
+               var value = "";
+                for (var i = 0; i < 9; i++) {
+                    if (i == 0 || i == 2 || i == 4 || i == 7) {
+                        value = n_array[i] * 10;
+                    } else {
+                        value = n_array[i];
+                    }
+                    if (value != 0) {
+                        words_string += words[value] + " ";
+                    }
+                    if ((i == 1 && value != 0) || (i == 0 && value != 0 && n_array[i + 1] == 0)) {
+                        words_string += "Crores ";
+                    }
+                    if ((i == 3 && value != 0) || (i == 2 && value != 0 && n_array[i + 1] == 0)) {
+                        words_string += "Lakhs ";
+                    }
+                    if ((i == 5 && value != 0) || (i == 4 && value != 0 && n_array[i + 1] == 0)) {
+                        words_string += "Thousand ";
+                    }
+                    if (i == 6 && value != 0 && (n_array[i + 1] != 0 && n_array[i + 2] != 0)) {
+                        words_string += "Hundred and ";
+                    } else if (i == 6 && value != 0) {
+                        words_string += "Hundred ";
+                    }
+                }
+                words_string = words_string.split("  ").join(" ");
+            }
+            return words_string;
         }
 
         vm.order.showItem = function () {
